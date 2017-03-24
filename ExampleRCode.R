90c75588eb9b98841b2e38f70b28130f9833e8ad
thrinadh <thrinadhn@gmail.com>
2017-03-24 14:38:34 +0530

    ExampleRcode

--- ExampleRCode.R
+++ ExampleRCode.R
@@ -1,18 +1,77 @@
-height <- c(176, 154, 138, 196, 132, 176, 181, 169, 150, 175)
-bodymass <- c(82, 49, 53, 112, 47, 69, 77, 71, 62, 78)
-plot(bodymass, height)
-plot(bodymass, height, pch = 16, cex = 1.3, col = "blue", main = "HEIGHT PLOTTED AGAINST BODY MASS", xlab = "BODY MASS (kg)", ylab = "HEIGHT (cm)")
-lm(height ~ bodymass)
-abline(lm(height ~ bodymass),col="red")
-abline(98.0054, 0.9528)
-par(mfrow = c(2,2))
-M <- lm(formula = height ~ bodymass)
-summary(M)
-0.0003662<=0.05
-plot(M)
-M2 <- lm(height ~ bodymass, subset=(1:length(height)!=5))
-summary(M2)
-library("git2r")
-library(gitlabr)
-library(githubinstall)
+install.packages("devtools",dependencies = TRUE)
 library(devtools)
+search()
+crimtab
+dim(crimtab)
+View(crimtab)
+str(crimtab)
+sum(crimtab)
+colnames(crimtab)
+apply(crimtab,2,var)
+pca =prcomp(crimtab)
+par(mar = rep(2, 4))
+plot(pca)
+pca$rotation=-pca$rotation
+pca$x=-pca$x
+biplot (pca , scale =0)
+#######################################
+x <- rnorm(100) 
+y <- 2 + 3 * x + rnorm(100) * 0.5 
+str(x) 
+model1 <- lm(y ~ x) 
+model1
+summary(model1)
+plot(x, y, main = "Simple linear regression") 
+abline(model1$coefficients, col = "blue") 
+#####################################
+x <- 1.5
+x
+numeric (10)
+c(1, 2, c(3, 4, 5))
+1:5
+seq(1, 10, 2)
+seq(3, length.out = 10)
+1 + 1:5
+TRUE
+1>2
+1<2
+c(1, 2) > 2
+c(1, 2) > c(2, 1)
+c(2, 3) > c(1, 2, -1, 3)
+1 %in% c(1, 2, 3)
+
+"hello, world!"
+'hello, world!'
+c("Hello", "World")
+c("Hello", "World") == c('Hello', 'World')
+c("Hello", "World") == "Hello, World"
+#cat("Is "You" a Chinese name?")
+cat('Is "You" a Chinese name?')
+
+v1 <- c(1, 2, 3, 4)
+v1[2]
+v1[2:4]
+v1[-3]
+a <- c(1, 3)
+v1[a]
+v1[c(1, 2, -3)]
+v1[3:6]
+v1[c(TRUE, FALSE, TRUE, FALSE)]
+v1[2] <- 0
+v1
+v1[2:4]
+v1
+v1[c(TRUE, FALSE, TRUE, FALSE)] <- c(3, 2)
+v1
+v1[v1 <= 2]
+#####################
+add <- function(x, y) {  x + y}
+x<-1
+y<-2\
+
+
+sum_sqrt<-function(x) {mean(x)}
+sum_sqrt(1:5)
+install.packages("installr")
+library(installr)
+updateR()
